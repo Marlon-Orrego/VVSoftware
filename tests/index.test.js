b17799746dde1ddc586d6f8a2117ffe806773b0b
Marlon-Orrego <marlonorrego118@gmail.com>
2023-05-26 20:55:35 -0500

    Correción Test

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../app");
 const Estudiante = require("../models/Estudiante");
 
-jest.mock("../models/Estudiante", () => {
+jest.mock("../src/models/Estudiante", () => {
   const mongoose = require("mongoose");
   const EstudianteSchema = new mongoose.Schema({
     nombre: String,
